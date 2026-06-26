a122fb516a3865cd238c4ddf0cabe5da073c064f
Clara Manolache <clara.manolache@gmail.com>
2026-06-26 08:33:12 -0700

    proj with unique, non-null names

---
+++ data population.sql
@@ -0,0 +1,29 @@
+USE acme_commercial_property;
+
+INSERT INTO Manufacturer (Name) VALUES
+    ('Aaon'),
+    ('Armstrong'),
+    ('Grundfos'),
+    ('Bell & Gossett'),
+    ('Condair'),
+    ('Cummins'),
+    ('Daikin'),
+    ('Greenheck'),
+    ('CaptiveAire'),
+    ('Huntair'),
+    ('IAC'),
+    ('Lochinvar'),
+    ('Mitsubishi'),
+    ('POWER QUALITY INTERNATIONAL'),
+    ('Price'),
+    ('Quincy'),
+    ('Southern Tier Technologies'),
+    ('Eaton'),
+    ('Siemens'),
+    ('GE'),
+    ('King Electric'),
+    ('Nordyne'),
+    ('Armstrong Air'),
+    ('Goodman / Amana'),
+    ('Midea')
+;
---
+++ tables creation.sql
@@ -0,0 +1,34 @@
+CREATE DATABASE IF NOT EXISTS acme_commercial_property;
+
+USE acme_commercial_property;
+
+CREATE TABLE Category (
+                          CategoryID INT AUTO_INCREMENT PRIMARY KEY,
+                          Name VARCHAR(150) NOT NULL UNIQUE
+);
+
+CREATE TABLE Manufacturer (
+                              ManufacturerID INT AUTO_INCREMENT PRIMARY KEY,
+                              Name VARCHAR(150)
+);
+
+CREATE TABLE Product (
+                         ProductID INT AUTO_INCREMENT PRIMARY KEY ,
+                         Name VARCHAR(300) NOT NULL,
+                         CategoryID INT NOT NULL,
+                         ManufacturerID INT NOT NULL,
+                         ModelNum VARCHAR(150),
+                         CONSTRAINT FK_Product_Category FOREIGN KEY (CategoryID)
+                             REFERENCES Category (CategoryID),
+                         CONSTRAINT FK_Product_Manufacturer FOREIGN KEY (ManufacturerID)
+                             REFERENCES Manufacturer (ManufacturerID)
+);
+
+CREATE TABLE Asset (
+                       AssetID INT AUTO_INCREMENT PRIMARY KEY,
+                       Name VARCHAR(300) NOT NULL,
+                       ProductID INT NOT NULL,
+                       SerialNumber INT,
+                       CONSTRAINT FK_Asset_Product FOREIGN KEY (ProductID)
+                           REFERENCES Product (ProductID)
+);
